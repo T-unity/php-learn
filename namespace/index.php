7482bf86e73d46a8ad9f226090bef56434d8662c
T-unity <takadanaritoshi@gmail.com>
2022-03-20 12:43:40 +0900

    cd

--- namespace/index.php
+++ namespace/index.php
@@ -1,8 +1,35 @@
 <?php
+require_once 'functions.php';
+require_once './namespace/index.php';
 
-namespace man;
+// function getGreeting() {
+//   echo 'ごきげんよう';
+// }
+// function getGreeting() {
+//   echo 'ガルルルル';
+// }
+// getGreeting();
 
-function getHeight()
-{
-  return '170';
-}
+echo man\getGreeting();
+echo "\n";
+echo man\getHeight();
+echo "\n";
+echo dog\getGreeting();
+echo "\n";
+
+// print_r(get_declared_classes());
+// print_r(get_defined_functions());
+
+use asia\JP\tohoku\aomori\man as tugarujin;
+
+echo tugarujin\getGreeting();
+echo "\n";
+
+require_once 'classes/human.php';
+
+use Amniota\Synapsida\Therapsida\Cynodontia\Hominina\Homo as human;
+
+$hoge = new human\human();
+var_dump($hoge);
+
+echo $hoge->getName();
